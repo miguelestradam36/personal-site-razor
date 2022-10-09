840ee95ecee9fc43b4020753c7c13c67a93a8d67
Miguel Estrada <miguelestradam36@estudiantec.cr>
2022-10-08 20:17:41 -0600

    Uploading changes

--- Pages/Index.razor.js
+++ Pages/Index.razor.js
@@ -1,12 +1,1 @@
-﻿// Activate Carousel
-$("#myCarousel").carousel();
-
-// Enable Carousel Indicators
-$(".item").click(function () {
-    $("#myCarousel").carousel(1);
-});
-
-// Enable Carousel Controls
-$(".left").click(function () {
-    $("#myCarousel").carousel("prev");
-});
+﻿
