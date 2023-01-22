ef6e014af1d4c677ce52208b282e52ae14fb64db
Miguel Estrada <miguelestradam36@estudiantec.cr>
2023-01-21 22:45:24 -0600

    Update

--- Data/Contact.cs
+++ Data/Contact.cs
@@ -15,19 +15,34 @@ namespace miguelestradam.Data
     {
         public void Send(string to, string subject, string html, string from = null)
         {
-            // create message
-            var email = new MimeMessage();
-            email.From.Add(MailboxAddress.Parse(from ?? "magnolia.koss@ethereal.email"));
-            email.To.Add(MailboxAddress.Parse(to));
-            email.Subject = subject;
-            email.Body = new TextPart(TextFormat.Html) { Text = html };
+            try
+            {
+                // create message
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse(from ?? "magnolia.koss@ethereal.email"));
+                email.To.Add(MailboxAddress.Parse(to));
+                email.Subject = subject;
+                email.Body = new TextPart(TextFormat.Html) { Text = html };
 
-            // send email
-            using var smtp = new SmtpClient();
-            smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
-            smtp.Authenticate("magnolia.koss@ethereal.email", "WASAAsMKdnBveqpw4n");
-            smtp.Send(email);
-            smtp.Disconnect(true);
+                // send email
+                using var smtp = new SmtpClient();
+                smtp.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+                smtp.Authenticate("magnolia.koss@ethereal.email", "WASAAsMKdnBveqpw4n");
+                smtp.Send(email);
+                smtp.Disconnect(true);
+            }
+            catch (SmtpCommandException e)
+            {
+                
+            }
+            catch (IOException e)
+            {
+                
+            }
+            catch
+            {
+                
+            }
         }
     }
 }
